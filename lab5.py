382313a0b164abfacbb5733acf489f4471383035
John McGettigan <john@mcgettigan.org>
2021-02-26 18:38:07 +0000

    Lab 5 If Else Elif

---
+++ lab5.py
@@ -0,0 +1,59 @@
+'''
+Lab 5
+'''
+
+#3.1
+#alien_color='red'
+#if alien_color=='green':
+#    print('You win 5 points!')
+    
+#3.2
+#if alien_color == 'green':
+#    print ('you just earned 5 points')
+#else:
+#    print('You just earned 10 points')
+    
+#3.3.
+
+# Checking if banana exists in list  
+# using loop 
+#favorite_fruits=['apple','banana','orange','bananas']
+#print("Checking if bananas in list ( using in ) : ") 
+#for i in favorite_fruits: 
+#    if(i == 'bananas') : 
+#        print ("Yes we have bananas!")
+#    else:
+#        print ("Yes, we have no bananas")
+        
+#if 'orange' in favorite_fruits:
+#    print ('you really like orange')
+  
+#if 'grapes' in favorite_fruits:
+#    print ('you really like grapes')
+    
+#if 'mango' in favorite_fruits:
+#    print ('you really like mango')   
+  
+
+#3.4
+my_kids = [ ['Eddie',12],
+            ['Sean',16],
+            ['Hannah',21]]
+            
+print(my_kids)
+
+
+#game_board = [ [0,0,0],
+#               [0,0,0],
+#               [0,0,0]]
+
+
+my_kidsage = 16
+
+if my_kidsage < 13:
+    print('You are a kid')
+elif my_kidsage <20:
+    print('You are a teenage')
+else:
+    print('You are an adult')
+
